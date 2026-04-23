f8d49af6e9377c16a45e542a861bdc67a8218464
Kauã Vargas <166948799+KauaBinello@users.noreply.github.com>
2026-04-22 21:11:59 -0300

    Update medicamentos.js

--- front/medicamentos/medicamentos.js
+++ front/medicamentos/medicamentos.js
@@ -4,7 +4,7 @@ if (localStorage.getItem('usuarioLogado') !== 'true') {
     // Para a execução de todo o resto do script
     throw new Error("Acesso não autorizado");
 }
-const API = 'https://api-medi-gestor.onrender.com';
+const API = 'https://apimedigestor.onrender.com';
 const limit = 12;
 let offset = 0;
 let idEmEdicao = null;
